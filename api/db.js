cef334c6d9d410590cf1191f304825240a22b978
Mayza Ester <130400582+maisazs@users.noreply.github.com>
2024-08-01 11:41:28 -0300

    Initial commit

---
+++ api/db.js
@@ -0,0 +1,10 @@
+//banco de dados
+
+import mysql from "mysql"
+
+export const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "3825",
+    database: "crud"
+})
